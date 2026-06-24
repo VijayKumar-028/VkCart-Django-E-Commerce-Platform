78693810afc12a41ac45be58320ba76b8b9a11a6
VijayKumar-028 <valivetivijaykumar05@gmail.com>
2026-06-24 14:09:57 +0530

    Added the Account Verification Process.

--- AmazeKart/settings.py
+++ AmazeKart/settings.py
@@ -138,3 +138,14 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     
 }
+
+#SMTP Configuration
+from decouple import config
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,19 @@
 
 
-import email
+
+from email.policy import default
 
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+#Verification Email
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 from accounts.forms import RegistrationForm
 
@@ -25,8 +34,23 @@ def register(request):
             user=Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password) # type: ignore
             user.phone_number=phone_number
             user.save()
-            messages.success(request, 'Registration Successful.')
-            return redirect('register')
+
+            #USER ACTIVATION
+            current_site=get_current_site(request)
+            mail_subject='Please activate your account'
+            message=render_to_string('accounts/account_verification_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':default_token_generator.make_token(user),
+            })
+            to_email=email
+            send_email=EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            
+
+            # messages.success(request, 'Thank You for registering with us, We have sent you a verification mail to your email address. Please verify it.')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form=RegistrationForm()
     context={
@@ -56,3 +80,22 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid=urlsafe_base64_decode(uidb64).decode()
+        user=Account._default_manager.get(pk=uid)
+    
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user=None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active=True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+
