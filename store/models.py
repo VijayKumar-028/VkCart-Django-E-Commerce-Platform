7f97cb0716cb9f767aa10c5e4df8e2bc886d3d5c
VijayKumar-028 <valivetivijaykumar05@gmail.com>
2026-08-24 22:15:35 +0530

    Review and Rating System, and Fixed the Anonnymous User Error.

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
-from itertools import product
+
 
 from django.db import models
+from django.db.models.aggregates import Avg, Count
 from django.urls import reverse
 
 from accounts.models import Account
@@ -20,7 +21,23 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+    
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(
+            product=self,
+            status=True
+        ).aggregate(average=Avg('rating'))
 
+        return round(reviews['average'] or 0, 2)
+
+
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(
+            product=self,
+            status=True
+        ).aggregate(count=Count('id'))
+
+        return reviews['count']
     def get_url(self):
         return reverse("product_detail", args=[self.category.slug, self.slug])
 
@@ -66,8 +83,18 @@ class ReviewRating(models.Model):
     ip=models.CharField(max_length=20, blank=True)
     status=models.BooleanField(default=True)
     created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.subject
 
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/products', max_length=255)
+
+    def __str__(self):
+        return self.product.product_name # type: ignore
+
+    class Meta:
+        verbose_name = 'productgallery'
+        verbose_name_plural = 'product gallery'
--- store/views.py
+++ store/views.py
@@ -11,9 +11,10 @@ from requests.utils import is_valid_cidr
 from carts.models import CartItem
 from carts.views import _cart_id
 from category.models import Category
+from orders.models import OrderProduct
 
 from .forms import ReviewForm
-from .models import Product, ReviewRating
+from .models import Product, ProductGallery, ReviewRating
 
 # Create your views here.
 
@@ -43,23 +44,36 @@ def store(request, category_slug=None):  # Bringing the store page with paginati
     return render(request, "store/store.html", context)
 
 #detailed products
-def product_detail(request, category_slug, product_slug):  # Every product detail
+def product_detail(request, category_slug, product_slug):
     try:
-        single_product = Product.objects.get(
-            category__slug=category_slug, slug=product_slug
-        )
-        in_cart = CartItem.objects.filter(
-            cart__cart_id=_cart_id(request), product=single_product
-        )
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
-    
+
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists() # type: ignore
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True) # type: ignore
+
+    # Get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id) # type: ignore
 
     context = {
-        "single_product": single_product,
-        "in_cart": in_cart,
+        'single_product': single_product,
+        'in_cart'       : in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews,
+        'product_gallery': product_gallery,
     }
-    return render(request, "store/product_detail.html", context)
+    return render(request, 'store/product_detail.html', context)
+  
 
 
 def search(request):  # Search Functionality by the keyword searched by the user
@@ -119,6 +133,7 @@ def submit_review(request, product_id):
                     request,
                     'Thank you! Your review has been submitted.'
                 )
+            
 
         return redirect(url)
 
